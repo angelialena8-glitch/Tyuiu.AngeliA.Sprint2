6e53d541842b5f1c8adcefa41708429eabe45f5b
ShaykhelislamovaDiana <Tyuiu.ShaykhelislamovaDA@outlook.com>
2026-04-05 20:06:05 +0500

    Task7

---
+++ Tyuiu.AngeliA.Sprint2.Task7.V4.Test/Test1.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.AngeliA.Sprint2.Task7.V4.Lib;
+namespace Tyuiu.AngeliA.Sprint2.Task7.V4.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.AngeliA.Sprint2.Task7.V4/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.AngeliA.Sprint2.Task7.V4.Lib;
+namespace Tyuiu.AngeliA.Sprint2.Task7.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
