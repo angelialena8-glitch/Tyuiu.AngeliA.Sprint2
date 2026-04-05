9137caa2773cb413fc30c08cff0c731acfb4bdf1
ShaykhelislamovaDiana <Tyuiu.ShaykhelislamovaDA@outlook.com>
2026-04-05 19:02:59 +0500

    Task1

---
+++ Tyuiu.AngeliA.Sprint2.Task1.V8.Lib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.AngeliA.Sprint2.Task1.V8.Lib
+{
+    public class Class1 : ISprint2Task1V8
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = [(a != b) | (c < d), (a == b) & (a != b), (a <= b) || (a != b), (a >= b) && (c <= d), !(b > a), (a <= b) ^ (c < d)];
+            return res;
+        }
+    }
+}
